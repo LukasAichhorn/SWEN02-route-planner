1b4f3d8ba2517420a8015ea1626072c7d9b95964
LukasAichhorn <lukas.aichhorn@gmail.com>
2022-05-23 12:17:57 +0200

    work on controller

--- src/main/java/at/fh/tourplanner/controller/Log/LogListController.java
+++ src/main/java/at/fh/tourplanner/controller/Log/LogListController.java
@@ -1,12 +1,24 @@
 package at.fh.tourplanner.controller.Log;
 
+import at.fh.tourplanner.model.Log;
 import at.fh.tourplanner.viewmodels.LogListViewModel;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
-public class LogListController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogListController implements Initializable {
     private final LogListViewModel logListViewModel;
+    public TableView<Log> logList;
 
     public LogListController(LogListViewModel logListViewModel) {
         this.logListViewModel = logListViewModel;
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
