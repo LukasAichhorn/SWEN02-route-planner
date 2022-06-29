04bc1c5b6c3e4b77354dce6c812c2f57afa26cf1
Haunzmcmahon <62951967+Haunzmcmahon@users.noreply.github.com>
2022-06-29 10:17:49 +0200

    interface and impl for PdfGeneration added, dummy pdf files for testing added

---
+++ src/main/java/at/fh/tourplanner/businessLayer/PdfGenerationService.java
@@ -0,0 +1,14 @@
+package at.fh.tourplanner.businessLayer;
+
+import at.fh.tourplanner.model.Tour;
+
+import java.util.List;
+
+public interface PdfGenerationService {
+
+
+    void generateTourReport(Tour tour);
+    void generateStatisticalReport(List<Tour> tours);
+
+
+}
